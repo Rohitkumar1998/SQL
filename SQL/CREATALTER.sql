cbb7c3c0e27b7b970013f8c447049aa96b0e681a
Rohitkumar1998 <rohitgupta199920@gmail.com>
2022-10-10 18:32:26 +0530

    sql

---
+++ SQL/CREATALTER.sql
@@ -0,0 +1,74 @@
+CREATE TABLE EMPLOYEE(
+    employeecode INT,
+    employeeFNAME VARCHAR(255),
+    employeeLname VARCHAR(50),
+-- maneger code to corect
+    managercode INT(10),
+    hiredate DATE NOT NULL,
+    -- SALARY DATA TYPE 
+    SALRY INT(10) NOT NULL,
+    COMMISION INT(10),
+    DEPTCODE INT(3) NOT NULL
+    -- EMAIL ADD 
+    );
+
+
+
+    -- ALTER COMMAND
+    --  USED FOR ADD, REMOVE,RENAME
+    --  IN COLU  ALTEMN
+    ALTER TABLE EMPLOYEE 
+    ADD COLUMN EMAIL VARCHAR(50) NOT NULL;
+    -- ADD AFTER 
+    ALTER TABLE EMPLOYEE
+
+    ADD COLUMN dob DATE 
+    AFTER COMMISION
+    -- ADD COLUMN FIRST
+    ALTER TABLE EMPLOYEE 
+    ADD COLUMN managerFNAME VARCHAR(50)
+    FIRST;
+    -- REMOVE
+    ALTER TABLE EMPLOYEE
+    DROP COLUMN managerFNAME;  
+    -- multiple remove 
+    ALTER TABLE 
+    DROP COLUMN COMMISION 
+    DROP COLUMN SALARY
+    -- RENAME 
+    ALTER TABLE 
+    CHANGE COLUMN COMMISION designation INT(10);
+    -- rename 
+    ALTER TABLE 
+    CHANGE COLUMN managercode ROHIT INT(8);
+-- CONSTRAINS
+-- ADD REMOVE MODIFY
+ALTER TABLE EMPLOYEE
+CHANGE COLUMN employeecode employeecode INT(4) UNIQUE NOT NULL;
+-- REMOVE
+    ALTER TABLE employee
+            CHANGE COLUMN  Salry Salry FLOAT; 
+   INSERT INTO EMPLOYEE  
+VALUES (9369, 'TONY', 'STARK', 'SOFTWARE ENGINEER', 7902, '1980-12-17', 2800,0,20,"abc@gmail.com"),
+       (9499, 'TIM', 'ADOLF', 'SALESMAN', 7698, '1981-02-20', 1600, 300,30,"abc@gmail.com"),    
+       (9566, 'KIM', 'JARVIS', 'MANAGER', 7839, '1981-04-02', 3570,0,20,"abc@gmail.com"),
+       (9654, 'SAM', 'MILES', 'SALESMAN', 7698, '1981-09-28', 1250, 1400, 30,"abc@gmail.com"),
+       (9782, 'KEVIN', 'HILL', 'MANAGER', 7839, '1981-06-09', 2940,0,10,"abc@gmail.com"),
+       (9788, 'CONNIE', 'SMITH', 'ANALYST', 7566, '1982-12-09', 3000,0,20,"abc@gmail.com"),
+       (9839, 'ALFRED', 'KINSLEY', 'PRESIDENT', 7566, '1981-11-17', 5000,0, 10,"abc@gmail.com"),
+       (9844, 'PAUL', 'TIMOTHY', 'SALESMAN', 7698, '1981-09-08', 1500,0,30,"abc@gmail.com"),
+       (9876, 'JOHN', 'ASGHAR', 'SOFTWARE ENGINEER', 7788, '1983-01-12',3100,0,20,"abc@gmail.com"),
+       (9900, 'ROSE', 'SUMMERS', 'TECHNICAL LEAD', 7698, '1981-12-03', 2950,0, 20,"abc@gmail.com"),
+       (9902, 'ANDREW', 'FAULKNER', 'ANAYLYST', 7566, '1981-12-03', 3000,0, 10,"abc@gmail.com"),
+       (9934, 'KAREN', 'MATTHEWS', 'SOFTWARE ENGINEER', 7782, '1982-01-23', 3300,0,20,"abc@gmail.com"),
+       (9591, 'WENDY', 'SHAWN', 'SALESMAN', 7698, '1981-02-22', 500,0,30,"abc@gmail.com"),
+       (9698, 'BELLA', 'SWAN', 'MANAGER', 7839, '1981-05-01', 3420, 0,30,"abc@gmail.com"),
+       (9777, 'MADII', 'HIMBURY', 'ANALYST', 7839, '1981-05-01', 2000, 200, NULL,"abc@gmail.com"),
+       (9860, 'ATHENA', 'WILSON', 'ANALYST', 7839, '1992-06-21', 7000, 100, 50,"abc@gmail.com"),
+       (9861, 'JENNIFER', 'HUETTE', 'ANALYST', 7839, '1996-07-01', 5000, 100, 50,"abc@gmail.com");
+    
+
+
+
+
+
